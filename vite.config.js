49939612478351580066564677cedd90d3b8d5eb
GreenMarioh <71488498+GreenMarioh@users.noreply.github.com>
2025-12-04 23:27:28 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/oh-whats-this/", // <--- THIS LINE IS CRITICAL
+  base: "/oh-whats-this/", // <--- This is the Critical Fix
 })
